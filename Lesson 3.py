dc934ddf98981fa7bb3009fc60eae494134ec345
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2022-12-07 19:20:38 +0100

    Z2J

---
+++ Lesson 3.py
@@ -0,0 +1,56 @@
+# STRINGS AND STRING METHODS
+#
+
+# string = "What i'm doing? "
+# print(string)
+# string1 = 'Hello World'
+# string2 = "1234"
+#
+# length = len(string1)
+# print(length)
+
+#multiline STRINGS (\)
+#
+# paragraph = "This string is very long problem is solved\
+# becouse i can use this slash\
+# and write code"
+#
+# print(paragraph)
+
+# paragraph2 = '''I can also use this triple cudzyslow if i wanna
+# multiline
+# and
+# long
+# shit
+#  but is no display on one line'''
+#
+# print(paragraph2)
+
+#Indexing and slicing
+
+
+#STRING CONCATENATION
+#
+# string1 = "abra"
+# string2 = "cadabra"
+# magic_string = string1 + string2
+# print(magic_string)
+
+
+#STRING Indexing
+#
+# string = "anything"
+# print(string[1])
+# print(string[5])
+# print(string[-1])
+# print(string[-1])
+# print(string[0])
+# user_input = 'anything'
+# #
+# # final_index = len(user_input) -1
+# # last_character = user_input[final_index]
+# # last_character = user_input[-1]
+# # print(last_character)
+
+#STRING slicing
+
