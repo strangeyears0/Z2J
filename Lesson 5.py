f7c0912036ab95e2a07b8695571de2c71f350003
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2022-12-17 12:03:50 +0100

    Z2J

--- Lesson 5.py
+++ Lesson 5.py
@@ -85,10 +85,80 @@
 
 # else keyword
 
-grade = 40
+# grade = 40
+#
+# if grade >= 70:
+#     print("you passed")
+# else:
+#     print("you dont pass")
+# print("No elo")
 
-if grade >= 70:
-    print("you passed")
-else:
-    print("you dont pass")
-print("No elo")
+# elif keyword
+
+# grade = 70
+#
+# if grade >=90:
+#     print("You pass with A")
+# elif grade >=80:
+#     print("You pass with B")
+# elif grade >=70:
+#     print("You pass with C")
+# else:
+#     print("You dont pass")
+# print("Thank you")
+
+#nested if statemnest
+
+# sport = input("Enter a sport")
+# p1_score = int(input("Enter player 1 score"))
+# p2_score = int(input("Enter player 2 score"))
+#
+# if sport.lower() == "basketball":
+#     if p1_score == p2_score:
+#         print("Draw")
+#     elif p1_score > p2_score:
+#         print("1 WIN")
+#     else:
+#         print("2 WIN")
+# elif sport.lower() == "golf":
+#     if p1_score == p2_score:
+#         print("Draw")
+#     elif p1_score < p2_score:
+#         print("1 WIN")
+#     else:
+#         print("2 WIN")
+# else:
+#     print("Uknow sport")
+
+#REFACTOR
+
+
+# sport = input("Enter a sport")
+# p1_score = int(input("Enter player 1 score"))
+# p2_score = int(input("Enter player 2 score"))
+#
+# sport = sport.lower()
+# if p1_score == p2_score:
+#     print("The game is a draw")
+# elif (sport == "basketball") or (sport == "golf"):
+#     p1_wins_bbal = (sport == "basketball") and (p1_score > p2_score)
+#     p1_wins_golf = (sport == "golf") and (p1_score < p2_score)
+#     p1_wins = p1_wins_bbal or p1_wins_golf
+#     if p1_wins:
+#         print("Player 1 Wins.")
+#     else:
+#         print("Player 2 wins.")
+# else:
+#     print("Uknown sport")
+
+# exercises1
+
+# user_input = input("Enter Word")
+# word_length = len(user_input)
+# if word_length == 5:
+#     print("Your input is 5 character long")
+# elif word_length > 5:
+#     print("Your input is greater than 5 characters long")
+# elif word_length < 5:
+#     print("Your input is less than 5 characters long")
+
