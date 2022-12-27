a85bcd3b91d939632716fb2b15da89c791217382
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2022-12-27 20:57:52 +0100

    Z2J

--- Lesson 5.py
+++ Lesson 5.py
@@ -1277,3 +1277,110 @@
 # }
 # print(states["California"])
 # print(states["California"]["flower"])
+
+# #exercises1.
+# # ex2
+# captains ={}
+# captains["Enterprise"]="Picard"
+# captains["Voyager"]="Janeway"
+# captains["Defiant"]="Sisko"
+#
+# print(captains)
+#
+# # ex3
+#
+# if "Enterprise" not in captains:
+#     captains["Enterprise"] = "uknown"
+# if "Discovery" not in captains:
+#     captains["Discovery"] = "uknown"
+# for ship in["Enterprise","Discovery"]:
+#     if not ship in captains:
+#         captains[ship] = "uknown"
+#
+# #ex4
+#
+# for ship, captain in captains.items():
+#     print(f"{ship} is captained by {captain}.")
+# # ex5
+# del captains["Discovery"]
+# print(captains)
+#
+# #ex6
+#
+# captains =dict(
+#     [
+#         ("Enterprise","Picard"),
+#         ("Voyager","Janeway"),
+#         ("Defiant","Sisko"),
+#     ]
+# )
+# print(captains)
+
+#9.7
+
+import random
+
+capitals_dict = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+}
+
+state, capital = random.choice(list(capitals_dict.items()))
+
+while True:
+    guess = input(f"What is the capital of {state}?").lower()
+    if guess == "exit":
+        print(f"The capital of {state} is {capital}.")
+        print("Goodbye")
+        break
+    elif guess == capital.lower():
+        print("Correct ")
+        break
