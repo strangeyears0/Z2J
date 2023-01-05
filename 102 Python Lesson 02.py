197519fbdd86d08782949759b19c822d711c8d7f
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-01-05 16:32:52 +0100

    Modules and Packages

---
+++ 102 Python Lesson 02.py
@@ -0,0 +1,16 @@
+# 11 Modules and packages
+
+import adder
+import adder as a
+# from adder import add, double
+# value = add(2,2)
+# print(value)
+# double_value = double(value)
+# print(double_value)
+
+
+# # ex1,2
+# from greeter import greet
+#
+# greet=greet("Real Python")
+# print(greet)
