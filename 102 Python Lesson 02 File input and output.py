128beca56a368abeefc82ca5067ae25848e6dd45
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-01-07 20:34:08 +0100

    Files

--- 102 Python Lesson 02 File input and output.py
+++ 102 Python Lesson 02 File input and output.py
@@ -7,3 +7,9 @@ path = pathlib.Path("/Users/David/Documents/hello.txt")
 # path = pathlib.Path("C:\Users\David\Desktop\hello.txt")
 path = pathlib.Path("C:/Users/David/Desktop/hello.txt")
 path = pathlib.Path(r"C:\Users\David\Desktop\hello.txt")
+
+
+home = pathlib.Path.home()
+print(home)
+print(pathlib.Path.cwd())
+print (home / "Desktop" / "hello.txt")
