bdf8adf04a361acb9ae6d0be0b247171c58d0185
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-01-07 17:18:55 +0100

    Files

---
+++ 102 Python Lesson 02 File input and output.py
@@ -0,0 +1,9 @@
+# Creating patho objects from strings
+
+import pathlib
+
+path = pathlib.Path("/Users/David/Documents/hello.txt")
+
+# path = pathlib.Path("C:\Users\David\Desktop\hello.txt")
+path = pathlib.Path("C:/Users/David/Desktop/hello.txt")
+path = pathlib.Path(r"C:\Users\David\Desktop\hello.txt")
