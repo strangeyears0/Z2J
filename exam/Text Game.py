6b452298c6a64f08d089e31b5c0eb6b1fdf92dbf
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-01-19 21:54:36 +0100

    Text Game

--- exam/Text Game.py
+++ exam/Text Game.py
@@ -1,11 +1,26 @@
+location = "room1"
+def room1():
+    file_path = "D:/Z2J/exam/room1.txt"
+    file = open(file_path, mode="r", encoding= "utf-8")
+    for line in file:
+        print(line,end="")
+def room2():
+    file_path = "D:/Z2J/exam/room2.txt"
+    file = open(file_path, mode="r", encoding= "utf-8")
+    for line in file:
+        print(line,end="")
 def help():
     print(f"u-use item\n"
           f"t-take item\n"
           f"i-item list\n"
           f"h-help\n"
-          f"d-describe\n")
-def describe():
-    pass
+          f"d-describe room\n"
+          f"l-go left\n"
+          f"r-go right\n")
+def describe_room1():
+    return room1()
+def describe_room2():
+    return room2()
 class Inventory:
 
     items = []
@@ -22,27 +37,31 @@ class Inventory:
 
         Inventory.items.append(self.name)
         print(f"You take {self.name} ")
+        print(f"You take {self.name} ")
 
 
 def action():
-    choice = input("Enter Choice")
+    choice = input("\nEnter Choice")
     choice.lower()
 
     if choice == 't':
         item = input("Choose item to take:\n")
         item.lower()
-
-        if item == "rock":
-            print(f"You take {item}")
-            return Inventory(item).items.append(item), action()
-        elif item == "paper":
-            print(f"You take {item}")
-            return Inventory(item).items.append(item), action()
-        elif item == "glass":
-            print(f"You take {item}")
-            return Inventory(item).items.append(item), action()
+        if location == "room1":
+            if item == "rock":
+                print(f"You take {item}")
+                return Inventory(item).items.append(item), action()
+            elif item == "paper":
+                print(f"You take {item}")
+                return Inventory(item).items.append(item), action()
+            elif item == "glass":
+                print(f"You take {item}")
+                return Inventory(item).items.append(item), action()
+            else:
+                return print(f"There is no such {item} here"), action()
         else:
-            return print(f"There is no such {item} here"), action()
+            print("Change location")
+            return action()
     elif choice == 'i':
         print(f"Your Inventory:\n"
               f"Inventory.items")
@@ -61,11 +80,19 @@ def action():
         return help(), action()
 
     elif choice == 'd':
-        return describe(), action()
+        return describe_room1(), action()
+    elif choice == 'l':
+        location = "room2"
+        if location == 'room2':
+
+        return describe_room2(),action()
     else:
         print("Bad choice! Type h if you wanna help")
         return action()
 
 
+
+
 help()
+describe_room1()
 action()
