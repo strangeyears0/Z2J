441eacce01d86a76daa68bf5b39ef6833c59ad44
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-02-16 21:26:45 +0100

    Working With Database Tables

---
+++ 103 Python Lesson 02.py
@@ -0,0 +1,54 @@
+import sqlite3
+# connection = sqlite3.connect("test_database.db")
+# print(type(connection))
+#
+# cursor = connection.cursor()
+# print(type(cursor))
+#
+# query ="SELECT datetime('now','localtime');"
+# results = cursor.execute(query)
+# print(results)
+# row = results.fetchone()
+# print(row)
+# time =row[0]
+# print(time)
+# connection.close()
+
+#USING WITH TO MANAGE YOUR DATABASE CONNECTION
+
+# with sqlite3.connect("test_database.db") as connection:
+#     cursor = connection.cursor()
+#     query = "SELECT datetime('now','localtime');"
+#     results = cursor.execute(query)
+#     row = results.fetchone()
+#     time = row[0]
+# print(time)
+
+#WORKING WITH DATABASE TABLES
+
+create_table = """
+CREATE TABLE People(
+    FirstName TEXT,
+    LastName TEXT,
+    Age INT
+    );"""
+insert_values ="""
+INSERT INTO People VALUES(
+    'Ron',
+    'Obvious',
+    42
+);"""
+
+connection = sqlite3.connect("test_database.db")
+cursor = connection.cursor()
+# cursor.execute(create_table)
+cursor.execute(insert_values)
+connection.commit()
+connection.close()
+
+connection = sqlite3.connect("test_database.db")
+cursor = connection.cursor()
+query = "SELECT * FROM People;"
+results = cursor.execute(query)
+fetch = results.fetchone()
+print(fetch)
