ce16a7d0aabd953e5fdeb2bc13ea90c1b3b1d519
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-02-18 13:15:52 +0100

    Avoid Security Issues With Parametrized Statemenets
    Retrieving Data

--- 103 Python Lesson 02.py
+++ 103 Python Lesson 02.py
@@ -74,20 +74,64 @@
 #     cursor.execute(insert_values)
 
 
-import sqlite3
-sql = """
-DROP TABLE IF EXISTS People;
-CREATE TABLE People(
-    FirstName TEXT,
-    LastName TEXT,
-    Age INT
-);
-INSERT INTO People VALUES(
-    'Ron',
-    'Obvious',
-    '42'
-);"""
+# import sqlite3
+# sql = """
+# DROP TABLE IF EXISTS People;
+# CREATE TABLE People(
+#     FirstName TEXT,
+#     LastName TEXT,
+#     Age INT
+# );
+# INSERT INTO People VALUES(
+#     'Ron',
+#     'Obvious',
+#     '42'
+# );"""
+#
+# with sqlite3.connect("test_database.db") as connection:
+#     cursor = connection.cursor()
+#     cursor.executescript(sql)
 
-with sqlite3.connect("test_database.db") as connection:
-    cursor = connection.cursor()
-    cursor.executescript(sql)
+# import sqlite3
+#
+# #Get person data from user
+#
+# first_name = input("Enter your first name:")
+# last_name = input("enter your last name:")
+# age = int(input("Enter your age:"))
+# data = (first_name,last_name,age)
+#
+#
+# with sqlite3.connect("test_database.db") as connection:
+#     cursor = connection.cursor()
+#     cursor.execute("INSERT INTO People VALUES(?,?,?);",data)
+#     cursor.execute("UPDATE People SET Age=? WHERE FirstName=? AND LastName=?;",(45,'Lugi','Italioan'))
+
+#RETRIEVING DATA
+
+# import sqlite3
+#
+# values = (
+#     ("Ron","Obvious",42),
+#     ("Luigi","Vercotti",43),
+#     ("Arthur","Belling",28)
+# )
+#
+# with sqlite3.connect("test_database.db") as connection:
+#     cursor = connection.cursor()
+#     cursor.execute("DROP TABLE IF EXISTS People")
+#     cursor.execute("""
+#     CREATE TABLE People(
+#         FirstName TEXT,
+#         LastName TEXT,
+#         Age INT
+#         );"""
+#     )
+#     cursor.executemany("INSERT INTO People VALUES(?,?,?);",values)
+#
+#     #SELECT ALL FIRST AND LAST NAMES FROM PEOPLE OVER AGE 30
+#     cursor.execute(
+#         "SELECT FirstName,LastName FROM People WHERE Age > 30;"
+#     )
+#     for row in cursor.fetchall():
+#         print(row)
