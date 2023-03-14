2a2ad82921b8c742867186cf3efbb76a8ef9e7bf
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-03-14 18:57:13 +0100

    Tkinter Controling Layout with geometry

--- Tkinter.py
+++ Tkinter.py
@@ -97,3 +97,102 @@
 # entry.insert(0,"What is your name?")
 # entry.pack()
 # window.mainloop()
+
+#18.6 Controling layout with geometry
+
+import tkinter as tk
+
+# window=tk.Tk()
+#
+# frame1 = tk.Frame(master=window,width=100,height=100,bg="red")
+# frame1.pack()
+#
+# frame2 = tk.Frame(master=window, width=50,height=50,bg="yellow")
+# frame2.pack()
+#
+# frame3 = tk.Frame(master=window, width=25,height=25,bg="blue")
+# frame3.pack()
+#
+# window.mainloop()
+
+# import tkinter as tk
+#
+# window=tk.Tk()
+#
+# frame1 = tk.Frame(master=window,height=100,bg="red")
+# frame1.pack(fill=tk.X)
+#
+# frame2 = tk.Frame(master=window,height=50,bg="yellow")
+# frame2.pack(fill=tk.X)
+#
+# frame3 = tk.Frame(master=window,height=25,bg="blue")
+# frame3.pack(fill=tk.X)
+#
+# window.mainloop()
+
+# import tkinter as tk
+#
+# window=tk.Tk()
+#
+# frame1 = tk.Frame(master=window,width=200,height=100,bg="red")
+# frame1.pack(fill=tk.Y,side=tk.LEFT,expand=True)
+#
+# frame2 = tk.Frame(master=window,width=100,bg="yellow")
+# frame2.pack(fill=tk.Y,side=tk.LEFT,expand=True)
+#
+# frame3 = tk.Frame(master=window,width=50,bg="blue")
+# frame3.pack(fill=tk.Y,side=tk.LEFT,expand=True)
+#
+# window.mainloop()
+#
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# frame=tk.Frame(master=window,width=150,height=150)
+# frame.pack()
+#
+# label1=tk.Label(master=frame,text="I'm at (0,0)",bg="red")
+# label1.place(x=0,y=0)
+#
+# label2=tk.Label(master=frame,text="I'm at (75,75)",bg="yellow")
+# label2.place(x=75,y=75)
+#
+# window.mainloop()
+
+# import tkinter as tk
+# window = tk.Tk()
+# for i in range(3):
+#     for j in range(3):
+#         frame = tk.Frame(
+#             master=window,
+#             relief=tk.RAISED,
+#             borderwidth=1
+#         )
+#         frame.grid(row=i, column=j,padx=5,pady=5)
+#         label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+#         label.pack(padx=5, pady=5)
+#
+# window.mainloop()
+#
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(3):
+    window.columnconfigure(i, weight=1, minsize=75)
+    window.rowconfigure(i, weight=1, minsize=50)
+
+    for j in range(0, 3):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack(padx=5, pady=5)
+
+window.mainloop()
+
