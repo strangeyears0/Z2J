47596d7eeac9da60afd6016c542e24e53bc20c93
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-03-16 18:00:00 +0100

    Tkinter Controling Layout with geometry

--- Tkinter.py
+++ Tkinter.py
@@ -175,24 +175,57 @@ import tkinter as tk
 #
 # window.mainloop()
 #
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# for i in range(3):
+#     window.columnconfigure(i, weight=1, minsize=75)
+#     window.rowconfigure(i, weight=1, minsize=50)
+#
+#     for j in range(0, 3):
+#         frame = tk.Frame(
+#             master=window,
+#             relief=tk.RAISED,
+#             borderwidth=1
+#         )
+#         frame.grid(row=i, column=j, padx=5, pady=5)
+#
+#         label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+#         label.pack(padx=5, pady=5)
+#
+# window.mainloop()
+
+# import tkinter as tk
+#
+# window = tk.Tk()
+# window.columnconfigure(0, minsize=250)
+# window.rowconfigure([0, 1], minsize=100)
+#
+# label1 = tk.Label(text="A")
+# label1.grid(row=0, column=0)
+#
+# label2 = tk.Label(text="B")
+# label2.grid(row=1, column=0)
+#
+# window.mainloop()
+
 import tkinter as tk
 
 window = tk.Tk()
 
-for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=75)
-    window.rowconfigure(i, weight=1, minsize=50)
+window.rowconfigure(0, minsize=50)
+window.columnconfigure([0, 1, 2, 3], minsize=50)
 
-    for j in range(0, 3):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        frame.grid(row=i, column=j, padx=5, pady=5)
+label1 = tk.Label(text="1", bg="black", fg="white")
+label2 = tk.Label(text="2", bg="black", fg="white")
+label3 = tk.Label(text="3", bg="black", fg="white")
+label4 = tk.Label(text="4", bg="black", fg="white")
 
-        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label.pack(padx=5, pady=5)
+label1.grid(row=0, column=0)
+label2.grid(row=0, column=1, sticky="ew")
+label3.grid(row=0, column=2, sticky="ns")
+label4.grid(row=0, column=3, sticky="nsew")
 
 window.mainloop()
 
