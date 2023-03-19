87c989bf56ca3448449699cde5935cc96539686d
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2023-03-19 19:49:01 +0100

    Temperature Conventer in Tkinter

---
+++ Temperature Converter.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+def fahrenheit_to_celcius():
+    """Convert the value from Fahrenheit to
+    Celcius and insert the
+    result into lbl_result"""
+
+    fahrenheit = ent_temperature.get()
+    celsius = (5/9)*(float(fahrenheit)-32)
+    lbl_result["text"]=f"{round(celsius)}\N{DEGREE CELSIUS}"
+window = tk.Tk()
+window.title("Temperature Converter")
+
+frm_entry = tk.Frame(master=window)
+ent_temperature=tk.Entry(master=frm_entry,width=10)
+lbl_temp = tk.Label(master=frm_entry,text="\N{DEGREE FAHRENHEIT}")
+
+ent_temperature.grid(row=0,column=0,sticky="e")
+lbl_temp.grid(row=0,column=1,sticky="w")
+
+btn_convert = tk.Button(
+    master=window,
+    text="\N{RIGHTWARDS BLACK ARROW}",
+    command=fahrenheit_to_celcius
+)
+lbl_result = tk.Label(master=window,text="\N{DEGREE CELSIUS}")
+
+frm_entry.grid(row=0,column=0,padx=10)
+btn_convert.grid(row=0,column=1,pady=10)
+lbl_result.grid(row=0,column=2,padx=10)
+
+window.mainloop()
